4a5447cf222e04c2e6fe1107bdfb03aea8dc4e2a
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-04 18:02:28 +0530

    added css to animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 console.log("hello game devs");
 
 const score = document.querySelector(".score");
@@ -6,7 +7,7 @@ const gameArea = document.querySelector(".gameArea");
 console.log(score);
 console.log(startScreen);
 console.log(gameArea);
-
+let player = {};
 const keys = {
   ArrowUp: false,
   ArrowDown: false,
@@ -17,11 +18,19 @@ const keys = {
 startScreen.addEventListener("click", Start);
 function gamePlay() {
   console.log("game play");
-  window.requestAnimationFrame(gamePlay);
+    if (player.start) {
+  window.requestAnimationFrame(gamePlay);}
 }
 function Start() {
+    gameArea.classList.remove('hide');  // remove hide class 
+    startScreen.classList.add('hide');   // add hide class to startScreen
+    player.start = true;
   console.log("game started");
   window.requestAnimationFrame(gamePlay);
+  let car = document.createElement("dev");
+  car.setAttribute("class", "car");
+//   car.innerText = "hey i am new attribute"
+    gameArea.appendChild(car);
 }
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
